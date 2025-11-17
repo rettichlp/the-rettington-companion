f803916543d302380d231bcccc822d7d2483dd87
RettichLP <rettichlp01@gmail.com>
2025-11-17 16:07:13 +0100

    Refactor Configuration to use shared GSON instance and remove redundant GsonBuilder setup

--- src/main/java/de/rettichlp/therettingtoncompanion/common/utils/ModUtils.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/common/utils/ModUtils.java
@@ -1,10 +1,29 @@
 package de.rettichlp.therettingtoncompanion.common.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
 import net.fabricmc.loader.api.FabricLoader;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.MOD_ID;
 
 public class ModUtils {
+
+    public static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .registerTypeAdapter(Instant.class, (JsonDeserializer<Instant>) (json, typeOfT, context) -> Instant.parse(json.getAsString()))
+            .registerTypeAdapter(Instant.class, (JsonSerializer<Instant>) (src, typeOfSrc, context) -> new JsonPrimitive(src.toString()))
+            .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, typeOfT, context) -> LocalDateTime.parse(json.getAsString()))
+            .registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>) (src, typeOfSrc, context) -> new JsonPrimitive(src.toString()))
+            .registerTypeAdapter(LocalTime.class, (JsonDeserializer<LocalTime>) (json, typeOfT, context) -> LocalTime.parse(json.getAsString()))
+            .registerTypeAdapter(LocalTime.class, (JsonSerializer<LocalTime>) (src, typeOfSrc, context) -> new JsonPrimitive(src.toString()))
+            .create();
 
     public static String getVersionString() {
         return FabricLoader.getInstance().getModContainer(MOD_ID)
