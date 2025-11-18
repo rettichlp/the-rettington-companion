df4d5af899e3f5c9eb6bb129f4e6830b6798c7b2
RettichLP <rettichlp01@gmail.com>
2025-11-18 22:51:12 +0100

    Adjust chat dimensions by modifying getWidth and getHeight methods for improved responsiveness

--- src/main/java/de/rettichlp/therettingtoncompanion/mixin/ChatHudMixin.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/mixin/ChatHudMixin.java
@@ -1,5 +1,6 @@
 package de.rettichlp.therettingtoncompanion.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.ChatHud;
@@ -16,6 +17,9 @@ import java.awt.Color;
 
 import static de.rettichlp.therettingtoncompanion.common.utils.TextUtils.checkForHighlightedMessageAndRun;
 import static de.rettichlp.therettingtoncompanion.common.utils.TextUtils.getString;
+import static java.lang.Math.max;
+import static net.minecraft.client.gui.hud.ChatHud.getHeight;
+import static net.minecraft.client.gui.hud.ChatHud.getWidth;
 
 @Mixin(ChatHud.class)
 public abstract class ChatHudMixin {
@@ -23,6 +27,9 @@ public abstract class ChatHudMixin {
     @Shadow
     @Final
     private MinecraftClient client;
+
+    @Shadow
+    public abstract boolean isChatFocused();
 
     @Inject(method = "method_71992",
             at = @At(value = "INVOKE",
@@ -49,5 +56,23 @@ public abstract class ChatHudMixin {
             // for some reason there is a 4px offset on the left side and 8px offset on the right side
             drawContext.fill(-4, y1, this.client.inGameHud.getChatHud().getWidth() + 8, y2, highlightColor);
         });
+    }
+
+    @ModifyReturnValue(method = "getWidth()I", at = @At("RETURN"))
+    private int moreWidth(int width) {
+        // from x = 0 to hotbar (length = 182)
+        int chatWidth = this.client.getWindow().getScaledWidth() / 2 - 91 - 12; // for some reason there is a 12px offset
+        double minecraftChatWidth = getWidth(this.client.options.getChatWidth().getValue());
+
+        return (int) max(chatWidth, minecraftChatWidth);
+    }
+
+    @ModifyReturnValue(method = "getHeight()I", at = @At("RETURN"))
+    private int moreFocusedHeight(int height) {
+        // half of the screen height
+        int chatHeight = this.client.getWindow().getScaledHeight() / 2;
+        double minecraftChatHeight = getHeight(this.client.options.getChatHeightFocused().getValue());
+
+        return isChatFocused() ? ((int) max(chatHeight, minecraftChatHeight)) : height;
     }
 }
