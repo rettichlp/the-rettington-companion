ec1c984a99459b56562c5dd5094c51d86b8d1f3a
RettichLP <rettichlp01@gmail.com>
2025-11-18 16:32:48 +0100

    Add ButtonEntry to ChatOptionTab for dynamic chat regex management

--- src/main/java/de/rettichlp/therettingtoncompanion/common/gui/screens/options/tabs/ChatOptionTab.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/common/gui/screens/options/tabs/ChatOptionTab.java
@@ -1,7 +1,10 @@
 package de.rettichlp.therettingtoncompanion.common.gui.screens.options.tabs;
 
+import de.rettichlp.therettingtoncompanion.common.gui.screens.components.scrollable.ButtonEntry;
 import de.rettichlp.therettingtoncompanion.common.gui.screens.components.scrollable.ChatRegexEntry;
 import de.rettichlp.therettingtoncompanion.common.gui.screens.components.scrollable.ScrollableListEntry;
+import de.rettichlp.therettingtoncompanion.common.gui.screens.components.scrollable.SectionEntry;
+import de.rettichlp.therettingtoncompanion.common.gui.screens.options.ModOptionScreen;
 import de.rettichlp.therettingtoncompanion.common.models.ChatRegex;
 import net.minecraft.text.Text;
 
@@ -10,6 +13,7 @@ import java.util.Collection;
 
 import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.configuration;
 import static java.util.regex.Pattern.compile;
+import static net.minecraft.text.Text.literal;
 import static net.minecraft.text.Text.translatable;
 
 public class ChatOptionTab extends AbstractOptionTab {
@@ -27,6 +31,9 @@ public class ChatOptionTab extends AbstractOptionTab {
     public Collection<ScrollableListEntry> getContent() {
         Collection<ScrollableListEntry> scrollableListEntries = new ArrayList<>();
 
+        SectionEntry sectionEntry = new SectionEntry(translatable("trc.option.chat.section.message_patterns.title"));
+        scrollableListEntries.add(sectionEntry);
+
         // default chat regex
         ChatRegex defaultChatRegex = new ChatRegex(compile(this.client.getGameProfile().name()), configuration.isDefaultChatRegex(), 0);
         ChatRegexEntry defaultChatRegexEntry = new ChatRegexEntry(defaultChatRegex, false);
@@ -36,6 +43,13 @@ public class ChatOptionTab extends AbstractOptionTab {
             ChatRegexEntry chatRegexEntry = new ChatRegexEntry(chatRegex, true);
             scrollableListEntries.add(chatRegexEntry);
         });
+
+        ButtonEntry buttonEntry = new ButtonEntry(literal("+"), button -> {
+            ChatRegex newChatRegex = new ChatRegex(compile(""), true, 0);
+            configuration.getChatRegexes().add(newChatRegex);
+            this.client.execute(() -> this.client.setScreen(new ModOptionScreen("chat")));
+        });
+        scrollableListEntries.add(buttonEntry);
 
         return scrollableListEntries;
     }
