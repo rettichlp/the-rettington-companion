27b28d231bf58477f7eb780c00e447e615bfa6e8
RettichLP <rettichlp01@gmail.com>
2025-11-19 10:20:37 +0100

    Add ModMenu integration and update option screen entry point

---
+++ src/main/java/de/rettichlp/therettingtoncompanion/common/integrations/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package de.rettichlp.therettingtoncompanion.common.integrations;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import de.rettichlp.therettingtoncompanion.common.gui.screens.options.ModOptionScreen;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> new ModOptionScreen();
+    }
+}
