1fe0893703d3894696ea1a8e7197c008d6869b28
RettichLP <rettichlp01@gmail.com>
2025-11-26 01:23:41 +0100

    Add tool durability warning and auto-restock functionality in ItemStackMixin

--- src/main/java/de/rettichlp/therettingtoncompanion/mixin/ItemStackMixin.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/mixin/ItemStackMixin.java
@@ -1,49 +1,112 @@
 package de.rettichlp.therettingtoncompanion.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.collection.DefaultedList;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.player;
 import static java.util.Comparator.comparingInt;
-import static net.minecraft.screen.slot.SlotActionType.QUICK_MOVE;
+import static net.minecraft.client.sound.PositionedSoundInstance.master;
+import static net.minecraft.screen.slot.SlotActionType.PICKUP;
+import static net.minecraft.sound.SoundEvents.BLOCK_NOTE_BLOCK_COW_BELL;
+import static net.minecraft.sound.SoundEvents.BLOCK_NOTE_BLOCK_IRON_XYLOPHONE;
+import static net.minecraft.sound.SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
+import static net.minecraft.text.Text.translatable;
+import static net.minecraft.util.Formatting.GOLD;
+import static net.minecraft.util.Formatting.GREEN;
+import static net.minecraft.util.Formatting.RED;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+
+    @Shadow
+    public abstract int getMaxDamage();
+
+    @Unique
+    private MinecraftClient client = MinecraftClient.getInstance();
 
     @Unique
     private Item item;
 
     @Inject(method = "useOnBlock", at = @At("HEAD"))
-    private void trc$useOnBlockHead(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+    private void trc$useOnBlockHead(@NotNull ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         this.item = context.getStack().getItem();
     }
 
     @Inject(method = "useOnBlock", at = @At("RETURN"))
-    private void trc$useOnBlockReturn(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
-        MinecraftClient client = MinecraftClient.getInstance();
+    private void trc$useOnBlockReturn(@NotNull ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        if (context.getWorld().isClient() && this.client.player != null && context.getStack().getCount() == 0) {
+            tryRestock(false);
+        }
+    }
 
-        ClientPlayerEntity player = client.player;
-        ItemStack currentStack = context.getStack();
+    @Inject(method = "onDurabilityChange", at = @At("HEAD"))
+    private void trc$onDurabilityChange(int damage, ServerPlayerEntity player, Consumer<Item> breakCallback, CallbackInfo ci) {
+        int remainingUses = getMaxDamage() - damage;
+        switch (remainingUses) {
+            case 50 -> {
+                player.sendMessage(translatable("trc.message.auto_restock.tool_durability_warning", this.item.getName().getString(), 50).formatted(GOLD), true);
+                this.client.getSoundManager().play(master(BLOCK_NOTE_BLOCK_COW_BELL.value(), 2f, 2f));
+            }
+            case 25 -> {
+                player.sendMessage(translatable("trc.message.auto_restock.tool_durability_warning", this.item.getName().getString(), 25).formatted(GOLD), true);
+                this.client.getSoundManager().play(master(BLOCK_NOTE_BLOCK_COW_BELL.value(), 2f, 2f));
+            }
+            case 10, 5 -> {
+                if (tryRestock(true)) {
+                    player.sendMessage(translatable("trc.message.auto_restock.restock_succeeded", this.item.getName().getString()).formatted(GREEN), true);
+                    this.client.getSoundManager().play(master(ITEM_ARMOR_EQUIP_GENERIC.value(), 1f, 2f));
+                } else {
+                    player.sendMessage(translatable("trc.message.auto_restock.restock_failed", this.item.getName().getString()).formatted(RED), true);
+                    this.client.getSoundManager().play(master(BLOCK_NOTE_BLOCK_COW_BELL.value(), 2f, 2f));
+                    this.client.getSoundManager().play(master(BLOCK_NOTE_BLOCK_IRON_XYLOPHONE.value(), 1f, 2f));
+                }
+            }
+        }
+    }
 
-        if (player == null || currentStack.getCount() > 0) {
-            return;
+    @Unique
+    private boolean tryRestock(boolean replace) {
+        Optional<ItemStack> optionalItemStack = getMatchingItemStack();
+
+        if (optionalItemStack.isEmpty()) {
+            return false;
         }
 
-        player.getInventory().getMainStacks().stream()
+        int slotWithStack = player.getInventory().getSlotWithStack(optionalItemStack.get());
+        int selectedSlot = player.getInventory().getSelectedSlot() + 36; // translate to screen handler slot index
+
+        if (replace) {
+            this.client.interactionManager.clickSlot(player.currentScreenHandler.syncId, selectedSlot, 0, PICKUP, player);
+        }
+
+        this.client.interactionManager.clickSlot(player.currentScreenHandler.syncId, slotWithStack, 0, PICKUP, player);
+        this.client.interactionManager.clickSlot(player.currentScreenHandler.syncId, selectedSlot, 0, PICKUP, player);
+
+        return true;
+    }
+
+    @Unique
+    private @NotNull Optional<ItemStack> getMatchingItemStack() {
+        DefaultedList<ItemStack> mainStacks = player.getInventory().getMainStacks();
+        return mainStacks.subList(9, mainStacks.size()).stream()
                 .filter(itemStack -> itemStack.isOf(this.item))
-                .min(comparingInt(ItemStack::getCount))
-                .ifPresent(itemStack -> {
-                    int slotWithStack = player.getInventory().getSlotWithStack(itemStack);
-                    // I don't know why, but this puts the item in the selected hotbar slot directly and not on the first free slot
-                    client.interactionManager.clickSlot(player.currentScreenHandler.syncId, slotWithStack, 0, QUICK_MOVE, player);
-                });
+                .filter(itemStack -> !itemStack.isDamageable() || itemStack.getMaxDamage() - itemStack.getDamage() > 10)
+                .min(comparingInt(ItemStack::getCount));
     }
 }
