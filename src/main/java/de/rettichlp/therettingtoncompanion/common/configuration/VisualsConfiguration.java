944fcbd7e676667067760dad24a045ea30350fde
RettichLP <rettichlp01@gmail.com>
2025-12-02 22:29:29 +0100

    Add custom day/night cycle configuration and time manipulation

--- src/main/java/de/rettichlp/therettingtoncompanion/common/configuration/VisualsConfiguration.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/common/configuration/VisualsConfiguration.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import net.minecraft.text.Text;
 
 import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.player;
+import static de.rettichlp.therettingtoncompanion.common.configuration.VisualsConfiguration.DayTimeValue.DT_OFF;
 import static de.rettichlp.therettingtoncompanion.common.configuration.VisualsConfiguration.EquipmentModelVisibility.ALL;
+import static net.minecraft.screen.ScreenTexts.OFF;
 import static net.minecraft.text.Text.empty;
 import static net.minecraft.text.Text.literal;
 import static net.minecraft.text.Text.translatable;
@@ -19,6 +21,7 @@ public class VisualsConfiguration {
     private boolean showArmorHud = true;
     private boolean showArrowHud = true;
     private EquipmentModelVisibility equipmentModelVisibility = ALL;
+    private DayTimeValue dayTimeValue = DT_OFF;
 
     @Getter
     @AllArgsConstructor
@@ -40,6 +43,36 @@ public class VisualsConfiguration {
                     .append(literal("Equipment").formatted(GRAY))
                     .append(literal(": ").formatted(DARK_GRAY))
                     .append(this.displayName), true);
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum DayTimeValue {
+
+        DT_OFF(-1),
+        DT_0(0),
+        DT_3000(3000),
+        DT_6000(6000),
+        DT_9000(9000),
+        DT_12000(12000),
+        DT_15000(15000),
+        DT_18000(18000),
+        DT_21000(21000);
+
+        private final int timeValue;
+
+        public Text getDisplayName() {
+            if (this == DT_OFF) {
+                return OFF;
+            }
+
+            return literal(String.valueOf(this.timeValue));
+        }
+
+        public DayTimeValue next() {
+            int nextOrdinal = ordinal() + 1;
+            return values()[nextOrdinal % values().length];
         }
     }
 }
