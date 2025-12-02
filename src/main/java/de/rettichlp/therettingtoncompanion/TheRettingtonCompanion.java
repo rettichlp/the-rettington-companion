f0cec2c3508ea09c938f8b39e804bafe87b85685
RettichLP <rettichlp01@gmail.com>
2025-12-02 17:19:10 +0100

    Add functionality to send world information to the player on join

--- src/main/java/de/rettichlp/therettingtoncompanion/TheRettingtonCompanion.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/TheRettingtonCompanion.java
@@ -4,7 +4,12 @@ import de.rettichlp.therettingtoncompanion.common.configuration.Configuration;
 import de.rettichlp.therettingtoncompanion.common.registry.Registry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,6 +39,32 @@ public class TheRettingtonCompanion implements ModInitializer {
             player = client.player;
 
             client.execute(this.registry::registerListeners);
+
+            if (configuration.chat().isKeepMessagesOnDisconnect()) {
+                sendWorldInfoOnJoin(client);
+            }
         });
+    }
+
+    /**
+     * Sends the world information (server name or save file name) to the player when they join a server or single-player world.
+     * Displays the world information as a chat message formatted with specific colors.
+     *
+     * @param client The Minecraft client instance. Used to determine whether the player is in a single-player world or a server, and
+     *               to retrieve the necessary information for the current world.
+     */
+    private void sendWorldInfoOnJoin(@NotNull MinecraftClient client) {
+        String worldName = "?";
+        if (client.isIntegratedServerRunning()) {
+            assert client.getServer() != null;
+            worldName = client.getServer().getSaveProperties().getLevelName();
+        } else if (client.getCurrentServerEntry() instanceof ServerInfo serverInfo) {
+            worldName = serverInfo.name.isBlank() ? serverInfo.address : serverInfo.name;
+        }
+
+        player.sendMessage(Text.empty()
+                .append(Text.literal("[").formatted(Formatting.GRAY))
+                .append(Text.literal(worldName).formatted(Formatting.AQUA))
+                .append(Text.literal("]").formatted(Formatting.GRAY)), false);
     }
 }
