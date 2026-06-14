92bd8945b10a1da23f563da0b9b0d86cfe9456bc
RettichLP <rettichlp01@gmail.com>
2026-06-15 01:11:40 +0200

    Style `OnOffCycleButtonEntry` options with colored text

--- src/main/java/de/rettichlp/therettingtoncompanion/gui/OnOffCycleButtonEntry.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/gui/OnOffCycleButtonEntry.java
@@ -6,6 +6,8 @@ import lombok.experimental.Accessors;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.network.chat.Component;
 
+import static net.minecraft.ChatFormatting.GREEN;
+import static net.minecraft.ChatFormatting.RED;
 import static net.minecraft.client.gui.components.Tooltip.create;
 import static net.minecraft.network.chat.CommonComponents.OPTION_OFF;
 import static net.minecraft.network.chat.CommonComponents.OPTION_ON;
@@ -16,8 +18,8 @@ import static net.minecraft.network.chat.Component.empty;
 @AllArgsConstructor
 public enum OnOffCycleButtonEntry implements ICycleButtonValue {
 
-    ON(OPTION_ON, create(empty())),
-    OFF(OPTION_OFF, create(empty()));
+    ON(OPTION_ON.copy().withStyle(GREEN), create(empty())),
+    OFF(OPTION_OFF.copy().withStyle(RED), create(empty()));
 
     private final Component value;
     private final Tooltip tooltip;
