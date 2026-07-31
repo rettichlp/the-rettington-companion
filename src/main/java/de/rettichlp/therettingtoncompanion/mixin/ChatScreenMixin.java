3d2525f6f212bcc8bce0edbc8999074aa2c96bb2
RettichLP <rettichlp01@gmail.com>
2026-07-31 18:11:50 +0200

    Update ChatScreenMixin to handle mouse interactions with copy buttons and context menu

--- src/main/java/de/rettichlp/therettingtoncompanion/mixin/ChatScreenMixin.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/mixin/ChatScreenMixin.java
@@ -79,6 +79,16 @@ public abstract class ChatScreenMixin extends Screen {
         double mouseX = event.x();
         double mouseY = event.y();
 
+        // check and handle mouse over context menu
+        if (this.copyButton != null && this.copyButton.isMouseOver(mouseX, mouseY)) {
+            cir.setReturnValue(this.copyButton.mouseClicked(event, doubleClick));
+            return;
+        } else if (this.copyButtonWithOutTimestamp != null && this.copyButtonWithOutTimestamp.isMouseOver(mouseX, mouseY)) {
+            cir.setReturnValue(this.copyButtonWithOutTimestamp.mouseClicked(event, doubleClick));
+            return;
+        }
+
+        // handle mouse over message
         GuiMessage hoveredGuiMessage = getHoveredGuiMessage(mouseX, mouseY);
         if (hoveredGuiMessage == null) {
             closeContextMenu();
@@ -87,14 +97,9 @@ public abstract class ChatScreenMixin extends Screen {
 
         if (event.button() == 1) {
             openContextMenu(hoveredGuiMessage, mouseX, mouseY);
+            cir.setReturnValue(true);
         } else {
-            if (this.copyButton != null && this.copyButton.isMouseOver(mouseX, mouseY)) {
-                cir.setReturnValue(this.copyButton.mouseClicked(event, doubleClick));
-            } else if (this.copyButtonWithOutTimestamp != null && this.copyButtonWithOutTimestamp.isMouseOver(mouseX, mouseY)) {
-                cir.setReturnValue(this.copyButtonWithOutTimestamp.mouseClicked(event, doubleClick));
-            } else {
-                closeContextMenu();
-            }
+            closeContextMenu();
         }
     }
 
