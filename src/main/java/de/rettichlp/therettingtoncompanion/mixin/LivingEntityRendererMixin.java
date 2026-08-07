e04ec24782817c2ca6bc66c48b94ae58371951aa
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-08-08 00:21:33 +0200

    Fix own nametag sometimes not showing due to scoreboard teams

--- src/main/java/de/rettichlp/therettingtoncompanion/mixin/LivingEntityRendererMixin.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/mixin/LivingEntityRendererMixin.java
@@ -1,9 +1,14 @@
 package de.rettichlp.therettingtoncompanion.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -20,5 +25,13 @@ public class LivingEntityRendererMixin {
             return original;
         }
         return configuration.visuals().isShowOwnNametag() ? null : original;
+    }
+
+    @Definition(id = "livingEntity", local = @Local(type = LivingEntity.class, argsOnly = true))
+    @Definition(id = "localPlayer", local = @Local(type = LocalPlayer.class))
+    @Expression("livingEntity != localPlayer")
+    @ModifyExpressionValue(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean trc$shouldShowNameExpression(boolean original) {
+        return configuration.visuals().isShowOwnNametag() || original;
     }
 }
