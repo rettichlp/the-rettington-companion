0ea145a8d4c0b1ceed8dfd803ba17461b53f99f3
RettichLP <rettichlp01@gmail.com>
2026-09-13 22:49:11 +0200

    Add UI and key binding for inventory slot lock feature

--- src/main/java/de/rettichlp/therettingtoncompanion/configuration/InventoryConfiguration.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/configuration/InventoryConfiguration.java
@@ -2,9 +2,13 @@ package de.rettichlp.therettingtoncompanion.configuration;
 
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 public class InventoryConfiguration {
 
     private boolean instantQuickMove = true;
     private boolean autoRestock = true;
+    private Set<Integer> lockedSlots = new HashSet<>();
 }
--- src/main/java/de/rettichlp/therettingtoncompanion/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/mixin/AbstractContainerScreenMixin.java
@@ -1,11 +1,14 @@
 package de.rettichlp.therettingtoncompanion.mixin;
 
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
+import net.minecraft.client.input.KeyEvent;
 import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import org.jspecify.annotations.NonNull;
@@ -15,10 +18,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.SLOT_LOCK_KEY;
 import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.configuration;
+import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.inventoryService;
 import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.player;
+import static net.minecraft.client.renderer.RenderPipelines.GUI_TEXTURED;
+import static net.minecraft.resources.Identifier.withDefaultNamespace;
 import static net.minecraft.world.inventory.ContainerInput.QUICK_MOVE;
 import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
@@ -28,9 +36,15 @@ import static org.spongepowered.asm.mixin.injection.At.Shift.AFTER;
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreenMixin<T extends AbstractContainerMenu> extends Screen implements MenuAccess<T> {
 
+    private static final Identifier LOCKED_SLOT_SPRITE = withDefaultNamespace("container/cartography_table/locked");
+
     @Shadow
     @Final
     protected T menu;
+
+    @Shadow
+    @Nullable
+    protected Slot hoveredSlot;
 
     protected AbstractContainerScreenMixin(Component title) {
         super(title);
@@ -61,4 +75,18 @@ public abstract class AbstractContainerScreenMixin<T extends AbstractContainerMe
     @Shadow
     @Nullable
     protected abstract Slot getHoveredSlot(double x, double y);
+
+    @Inject(method = "extractSlot", at = @At("TAIL"))
+    private void trc$extractSlotTail(GuiGraphicsExtractor graphics, Slot slot, int mouseX, int mouseY, CallbackInfo ci) {
+        if (inventoryService.isLockedSlot(slot)) {
+            graphics.blitSprite(GUI_TEXTURED, LOCKED_SLOT_SPRITE, slot.x + 10, slot.y, 6, 8);
+        }
+    }
+
+    @Inject(method = "keyPressed", at = @At("HEAD"))
+    private void trc$keyPressedHead(KeyEvent event, CallbackInfoReturnable<Boolean> cir) {
+        if (SLOT_LOCK_KEY.matches(event)) {
+            inventoryService.onSlotLockKeyPressed(this.hoveredSlot);
+        }
+    }
 }
