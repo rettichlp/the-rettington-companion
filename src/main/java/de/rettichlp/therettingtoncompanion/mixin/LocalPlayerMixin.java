97e2583299ae3738e1f00cb2d3b95f404d4816bb
RettichLP <rettichlp01@gmail.com>
2026-09-25 00:54:21 +0200

    Add LocalPlayerMixin to prevent dropping items from locked inventory slots

---
+++ src/main/java/de/rettichlp/therettingtoncompanion/mixin/LocalPlayerMixin.java
@@ -0,0 +1,20 @@
+package de.rettichlp.therettingtoncompanion.mixin;
+
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.inventoryService;
+
+@Mixin(LocalPlayer.class)
+public abstract class LocalPlayerMixin {
+
+    @Inject(method = "drop", at = @At("HEAD"), cancellable = true)
+    private void trc$dropHead(boolean all, CallbackInfoReturnable<Boolean> cir) {
+        if (inventoryService.isLockedSlot(((LocalPlayer) (Object) this).getInventory().getSelectedSlot())) {
+            cir.setReturnValue(false);
+        }
+    }
+}
