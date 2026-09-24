02dba7b6dfab112c693e1b4109df8caf1f76aa6b
RettichLP <rettichlp01@gmail.com>
2026-09-24 19:21:15 +0200

    Add support for dynamic custom crosshair textures in VisualsService

--- src/main/java/de/rettichlp/therettingtoncompanion/services/VisualsService.java
+++ src/main/java/de/rettichlp/therettingtoncompanion/services/VisualsService.java
@@ -4,10 +4,45 @@ import com.mojang.blaze3d.platform.NativeImage;
 import de.rettichlp.therettingtoncompanion.mixin.OverlayTextureAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.resources.Identifier;
+import org.jspecify.annotations.Nullable;
 
+import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.MOD_ID;
 import static de.rettichlp.therettingtoncompanion.TheRettingtonCompanion.configuration;
+import static de.rettichlp.therettingtoncompanion.configuration.VisualsConfiguration.CUSTOM_CROSSHAIR_SIZE;
+import static net.minecraft.resources.Identifier.fromNamespaceAndPath;
 
 public class VisualsService {
+
+    public static final Identifier CUSTOM_CROSSHAIR_TEXTURE = fromNamespaceAndPath(MOD_ID, "dynamic/custom_crosshair");
+
+    private @Nullable DynamicTexture customCrosshairTexture;
+
+    public Identifier getCustomCrosshairTextureId() {
+        if (this.customCrosshairTexture == null) {
+            refreshCustomCrosshairTexture();
+        }
+
+        return CUSTOM_CROSSHAIR_TEXTURE;
+    }
+
+    public void refreshCustomCrosshairTexture() {
+        int[] pixels = configuration.visuals().getCustomCrosshairPixels();
+
+        if (this.customCrosshairTexture == null) {
+            this.customCrosshairTexture = new DynamicTexture(() -> "custom_crosshair", new NativeImage(CUSTOM_CROSSHAIR_SIZE, CUSTOM_CROSSHAIR_SIZE, true));
+            Minecraft.getInstance().getTextureManager().register(CUSTOM_CROSSHAIR_TEXTURE, this.customCrosshairTexture);
+        }
+
+        NativeImage image = this.customCrosshairTexture.getPixels();
+        for (int y = 0; y < CUSTOM_CROSSHAIR_SIZE; y++) {
+            for (int x = 0; x < CUSTOM_CROSSHAIR_SIZE; x++) {
+                image.setPixel(x, y, pixels[y * CUSTOM_CROSSHAIR_SIZE + x]);
+            }
+        }
+
+        this.customCrosshairTexture.upload();
+    }
 
     public int getDamageOverlayColor() {
         int rgb = configuration.visuals().getDamageOverlayColor() & 0x00FFFFFF; // isolate RGB
